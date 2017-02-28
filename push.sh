35d89eb6ce1ae97bb0d6bfbab75f3b9e413d156e
Devraj Mehta <devm33@gmail.com>
2017-02-28 14:57:28 -0500

    remove unused

--- push.sh
+++ push.sh
@@ -3,8 +3,6 @@
 DEST='devrajm/cast-sample-sender'
 BUCK="gs://$DEST"
 
-gsutil cp -p index.html $BUCK/index.html
-
-# gsutil acl ch -u 'AllUsers:R' $BUCK/index.html
+gsutil cp index.html $BUCK/index.html
 
 open "https://storage.googleapis.com/$DEST/index.html"
