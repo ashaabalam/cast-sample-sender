6409beaaf755edcef1c5bb515eaf2240452ad4ec
Devraj Mehta <devm33@gmail.com>
2017-02-28 14:37:39 -0500

    remove references to deleted js

--- push.sh
+++ push.sh
@@ -3,10 +3,8 @@
 DEST='devrajm/cast-sample-sender'
 BUCK="gs://$DEST"
 
-gsutil cp index.html $BUCK/index.html
-gsutil cp sender.js $BUCK/sender.js
+gsutil cp -p index.html $BUCK/index.html
 
-gsutil acl ch -u 'AllUsers:R' $BUCK/index.html
-gsutil acl ch -u 'AllUsers:R' $BUCK/sender.js
+# gsutil acl ch -u 'AllUsers:R' $BUCK/index.html
 
 open "https://storage.googleapis.com/$DEST/index.html"
