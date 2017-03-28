aafcf70521ec2c624c3c9d9920f2f771aa3db58e
Devraj Mehta <devm33@gmail.com>
2017-03-27 20:10:38 -0400

    update push dest and only open sometimes

--- push.sh
+++ push.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-DEST='devrajm/cast-sample-sender'
+# DEST='devrajm/cast-sample-sender'
+DEST='devrajm/cast-sender'
 BUCK="gs://$DEST"
 
 gsutil cp index.html $BUCK/index.html
 
-open "https://storage.googleapis.com/$DEST/index.html"
+if [ -z $1 ]; then
+  open "https://storage.googleapis.com/$DEST/index.html"
+  echo 'pass an argument to prevent re-opening'
+fi
